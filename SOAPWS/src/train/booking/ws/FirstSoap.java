3a24b968451a1d785f203bf3f96840db536935a0
Maxime CARDINALE <maxime.cardinale@ensiie.fr>
2023-09-08 13:50:34 +0200

    Setup SOAP and REST projects

---
+++ SOAPWS/src/train/booking/ws/FirstSoap.java
@@ -0,0 +1,21 @@
+package train.booking.ws;
+
+public class FirstSoap {
+	
+	public String sayHello(String input){
+		return "Hello "+input;
+	}	
+	
+	// Instantiate a REST Client
+	// Makes requests to the REST API
+	
+	// User Authentication
+	public void login() {
+		return;
+	}
+	
+	public void logout() {
+		return;
+	}
+
+}
