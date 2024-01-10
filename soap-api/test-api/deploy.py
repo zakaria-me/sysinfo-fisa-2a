a5a33acbe72b8b0be5693d689ef5735ebd43c8bb
Zakaria MELLAH <zakaria.mellah.pro@gmail.com>
2024-01-10 10:33:54 +0100

    delete some unused dev artefact

--- soap-api/test-api/deploy.py
+++
@@ -1,7 +0,0 @@
-from wsgiref.simple_server import make_server
-
-if __name__ == '__main__':
-    server = make_server('127.0.0.1', 8000, WsgiApplication(application))
-    print("SOAP service is running on http://127.0.0.1:8000")
-    print("WSDL is at: http://127.0.0.1:8000/?wsdl")
-    server.serve_forever()
